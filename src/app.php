835546fb32e223ed1f0c81ae83753bfa69507c0e
Bruno Renato de Sousa Rodrigues <bruno.renato_de_sousa_rodrigues@contractors.roche.com>
2023-01-11 01:14:40 +0000

    raw

--- src/app.php
+++ src/app.php
@@ -1,36 +1,5 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/db-connect.php';
-
-use Silex\Application;
-use Silex\Provider\TwigServiceProvider;
-use Symfony\Component\HttpFoundation\Request;
-
-// Setup the application
-$app = new Application();
-$app->register(new TwigServiceProvider, array(
-    'twig.path' => __DIR__ . '/templates',
-));
-
-// Setup the database
-$app['db.table'] = DB_TABLE;
-$app['db.dsn'] = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
-$app['db'] = $app->share(function ($app) {
-    return new PDO($app['db.dsn'], DB_USER, DB_PASSWORD);
-});
-
-// Handle the index page
-$app->match('/', function () use ($app) {
-    $query = $app['db']->prepare("SELECT message, author FROM {$app['db.table']}");
-    $thoughts = $query->execute() ? $query->fetchAll(PDO::FETCH_ASSOC) : array();
-
-    return $app['twig']->render('index.twig', array(
-        'title'    => 'Your Thoughts',
-        'thoughts' => $thoughts,
-    ));
-});
-
 
 #######
 $conn = new mysqli('terraform-20230108221845171900000001.csfl3dafisa1.eu-central-1.rds.amazonaws.com', 'admin', 'ohayPgeefVYbWnovSHu99iVQ', 'mydb');
@@ -42,11 +11,14 @@ if ($conn->connect_error) {
 $sql = "SELECT author, message FROM urler";
 $result = $conn->query($sql);
 
+print_r($result);
+
+print_r($result->fetch_assoc())
 
 if ($result->num_rows > 0) {
       // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "author: " . $row["id"]. " - Message: " . $row["message"]. "<br>";
+        echo "author: " . $row["author"]. " - Message: " . $row["message"]. "<br>";
     }
 } else {
     echo "0 results";
@@ -55,42 +27,4 @@ $conn->close();
 
 #######
 
-// Handle the add page
-$app->match('/add', function (Request $request) use ($app) {
-    $alert = null;
-    // If the form was submitted, process the input
-    if ('POST' == $request->getMethod()) {
-        try {
-            // Make sure the photo was uploaded without error
-            $message = $request->request->get('thoughtMessage');
-            $author = $request->request->get('thoughtAuthor');
-            if ($message && $author && strlen($author) < 64) {
-                // Save the thought record to the database
-                $sql = "INSERT INTO {$app['db.table']} (message, author) VALUES (:message, :author)";
-                $query = $app['db']->prepare($sql);
-                $data = array(
-                    ':message' => $message,
-                    ':author'  => $author,
-                );
-                if (!$query->execute($data)) {
-                    throw new \RuntimeException('Saving your thought to the database failed.');
-                }
-            } else {
-                throw new \InvalidArgumentException('Sorry, The format of your thought was not valid.');
-            }
-
-            // Display a success message
-            $alert = array('type' => 'success', 'message' => 'Thank you for sharing your thought.');
-        } catch (Exception $e) {
-            // Display an error message
-            $alert = array('type' => 'error', 'message' => $e->getMessage());
-        }
-    }
-
-    return $app['twig']->render('add.twig', array(
-        'title' => 'Share Your Thought!',
-        'alert' => $alert,
-    ));
-});
-
-$app->run();
+?>
