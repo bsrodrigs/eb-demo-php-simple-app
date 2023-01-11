2137f9be69cff3941fae37ee41a4f055bd28d535
Bruno Renato de Sousa Rodrigues <bruno.renato_de_sousa_rodrigues@contractors.roche.com>
2023-01-11 10:48:21 +0000

    Rem creds, prettify

--- src/app.php
+++ src/app.php
@@ -1,29 +1,43 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                table, th, td {
+                border: 1px solid white;
+                border-collapse: collapse;
+                }
+                th, td {
+                background-color: #96D4D4;
+                }
+            </style>
+        </head>
+    <body>
 <?php
 
-
-#######
-$conn = new mysqli('terraform-20230108221845171900000001.csfl3dafisa1.eu-central-1.rds.amazonaws.com', 'admin', 'ohayPgeefVYbWnovSHu99iVQ', 'mydb');
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT author, message FROM urler";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    echo "<table><tr><th>Author</th><th>Message</th></tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["author"]." ".$row["message"]."</td></tr>";
+    $conn = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME']);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
     }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
 
-$conn->close();
+    $sql = "SELECT author, message FROM urler";
+    $result = $conn->query($sql);
 
-#######
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>Author</th><th>Message</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["author"]."</td><td>".$row["message"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
 
-?>
+    $conn->close();
+
+    ?>
+
+    </body>
+</html>
+
