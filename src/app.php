9d832ba250a57b885b0a8151f413a9d4aeccb0eb
Bruno Renato de Sousa Rodrigues <bruno.renato_de_sousa_rodrigues@contractors.roche.com>
2023-01-11 01:22:45 +0000

    raw4

--- src/app.php
+++ src/app.php
@@ -2,7 +2,7 @@
 
 
 #######
-$conn = new mysqli('terraform-20230108221845171900000001.csfl3dafisa1.eu-central-1.rds.amazonaws.com', 'admin', 'ohayPgeefVYbWnovSHu99iVQ', 'mydb');
+$conn = new mysqli('terraform-20230108221845171900000001.csfl3dafisa1.eu-central-1.rds.amazonaws.com', DB_USER, 'ohayPgeefVYbWnovSHu99iVQ', DB_NAME);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -14,6 +14,17 @@ $result = $conn->query($sql);
 print_r($result);
 
 print_r($result->fetch_assoc());
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["author"]." ".$row["message"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
 
 $conn->close();
 
